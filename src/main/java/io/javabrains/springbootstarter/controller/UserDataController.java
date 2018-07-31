bf3ed2c72d803c8a040a8982206e6b1c4c688652
SVAM\sanchit.aggarwal <sanchit.aggarwal@svam.com>
2018-07-31 21:23:46 +0530

    remove path name

--- src/main/java/io/javabrains/springbootstarter/controller/UserDataController.java
+++ src/main/java/io/javabrains/springbootstarter/controller/UserDataController.java
@@ -26,7 +26,7 @@ import java.util.List;
 @Controller
 public class UserDataController implements UserDataControllerIfc{
 
-    private static final String FILE_PATH = "D:\\Employee-Timesheet-System\\WeeklyTimesheet.xlsx";
+   
 
 
     /**
